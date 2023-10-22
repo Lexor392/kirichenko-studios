5164f16d4a2cc872f269817ef09de301ef77860a
Lexor <lesha.goloborodko392@gmail.com>
2023-10-22 19:20:22 +0300

    Добавленны новые фишки

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -32,3 +32,60 @@ $(document).ready(function () {
 
     $('#maindiv').width($('#div1').width());
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    class Bubble {
+        constructor() {
+            this.elems = [
+                {
+                    el: document.querySelector('.bubble'),
+                    factor: 0.8
+                },
+                {
+                    el: document.querySelector('.bubble-out'),
+                    factor: 0.4
+                }
+            ]
+            this.holder = document.querySelector('.holder')
+            this.duration = 0.6
+            this.leaveDuration = 1
+
+            this.addEventListeners()
+        }
+
+        addEventListeners() {
+            this.holder.addEventListener('mousemove', this.mouseMove.bind(this))
+            this.holder.addEventListener('mouseleave', this.mouseLeave.bind(this))
+        }
+
+        mouseMove(e) {
+            const rawX = e.offsetX
+            const rawY = e.offsetY
+            const w = this.holder.offsetWidth
+            const h = this.holder.offsetHeight
+            const halfW = w / 2
+            const halfH = h / 2
+
+            this.elems.map((item) => {
+                const x = (rawX - halfW) * item.factor
+                const y = (rawY - halfH) * item.factor
+
+                TweenMax.to(item.el, this.duration, {
+                    x: x,
+                    y: y
+                })
+            })
+        }
+
+        mouseLeave() {
+            this.elems.map((item) => {
+                TweenMax.to(item.el, this.leaveDuration, {
+                    x: 0,
+                    y: 0
+                })
+            })
+        }
+    }
+
+    const bubble = new Bubble()
+});
